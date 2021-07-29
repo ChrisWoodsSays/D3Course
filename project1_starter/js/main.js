4c6b6e97482281bc612cf6a587faeb0877aae0e3
Chris Woods <42043980+ChrisWoodsSays@users.noreply.github.com>
2021-07-29 18:23:06 +0100

    Project 1.3 - Bubble Chart

--- project1_starter/js/main.js
+++ project1_starter/js/main.js
@@ -11,6 +11,7 @@ const topRockAlbums = [
  .append('h3')
     .text('Top Rock Albums');
 
+// Create SVG for Bar Chart
 const barChartWidth = 500;
 const barChartHeight = 130;
 const barChart = topAlbumsSection
@@ -20,6 +21,7 @@ const barChart = topAlbumsSection
         .attr('height', barChartHeight);
 
 const marginLeft = 200;
+// Add vertical line (kinda Y axis)
 barChart
     .append('line')
         .attr('x1', marginLeft)
@@ -29,6 +31,7 @@ barChart
         .attr('stroke', '#333')
         .attr('stroke-width', 2);
 
+// Create Bar Chart Linear Scale
 const barLengthScale = d3.scaleLinear()
     .domain([0, 1000000]) // In our data, the number of album-equivalent goes up to about 1,000,000
     .range([0, barChartWidth - marginLeft - 100]); // Based on the space that we have on screen and the space we need for the labels
@@ -42,13 +45,13 @@ const barMargin = 5;
 barChart.selectAll('rect')
     .data(topRockAlbums)
     .join('rect')
-        //.attr('width', d => barLengthScale(d.eq_albums))
+        .attr('width', d => barLengthScale(d.eq_albums))
         // re-write the width .attr() function so we can add the
         // console log to see the whole contents of each datum (d)
-        .attr('width', d => {
-            console.log(d);
-            return barLengthScale(d.eq_albums)
-         })
+        // .attr('width', d => {
+        //     console.log(d);
+        //     return barLengthScale(d.eq_albums)
+        //  })
         .attr('height', barThickness)
         .attr('x', marginLeft + 1)
         .attr('y', (d, i) => barMargin + (barThickness + barMargin) * i)
@@ -75,3 +78,70 @@ barChart.selectAll('.label-artist-album')
         .attr('x', 10)
         .attr('y', (d, i) => (barMargin + (barThickness + barMargin) * i) + 14)
         .text(d => d.artist + ", " + d.title);
+
+const topSongsSection = d3.select('#top-songs');
+topSongsSection
+    .append('p')
+    .append('h3')
+        .text('Top Rock Songs');
+
+// Create SVG for Circles Chart
+const circlesChartWidth = 600;
+const circlesChartHeight = 140;
+const circlesChart = topSongsSection
+    .append('svg')
+        .attr('viewbox', [0, 0, circlesChartWidth, circlesChartHeight])
+        .attr('width', circlesChartWidth)
+        .attr('height', circlesChartHeight);
+
+// Add horizotnal line (kinda X axis)
+circlesChart
+    .append('line')
+        .attr('x1', 0)
+        .attr('y1', circlesChartHeight / 2)
+        .attr('x2', circlesChartWidth)
+        .attr('y2', circlesChartHeight / 2)
+        .attr('stroke', '#333')
+        .attr('stroke-width', 1);
+
+// Add Circle Groups
+// Select them using the human readable class name of g elements
+circlesChartGroup = circlesChart.selectAll('.circleGroup')
+    .data(topRockSongs)
+    .join('g')
+        .attr('class', 'circle-group');
+
+// Create Circle Linear Scale
+// Circles should always be sized based on their area, not their radius!
+const radiusMax = 40;
+const circlesScale = d3.scaleLinear()
+    .domain([0, d3.max(topRockSongs, d => d.sales_and_streams)]) 
+    .range([0, Math.PI * Math.pow(radiusMax, 2)]);
+
+// We can now APPEND circles to the Groups
+const marginMargin = 15;
+circlesChartGroup
+    .append('circle')
+        .attr('r', d => Math.sqrt(circlesScale(d.sales_and_streams) / Math.PI))
+        .attr('cx', (d, i) => radiusMax + marginMargin + (i * 2 * (radiusMax + marginMargin)))
+        .attr('cy', circlesChartHeight / 2)
+        .attr('fill', '#a6d854');
+
+// Add APPEND Songs Sold Labels
+circlesChartGroup
+    .append('text')
+        .attr('class', 'label label-sales')
+        .attr('x', (d, i) => radiusMax + marginMargin + (i * 2 * (radiusMax + marginMargin)))
+        .attr('y', circlesChartHeight - 5)
+        .attr('text-anchor', 'middle')
+        .text(d => d.sales_and_streams/ 1000000 + 'M');
+
+// Add APPEND Song Title Labels
+circlesChartGroup
+    .append('text')
+        .attr('class', 'label label-titles')
+        .attr('x', (d, i) => radiusMax + marginMargin + (i * 2 * (radiusMax + marginMargin)))
+        .attr('y', 20)
+        .attr('text-anchor', 'middle')
+        .text(d => d.title);
+
